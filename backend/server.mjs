710a70966026e07781c51859afbf0811ae816a1f
madhu-111 <kanakadrga577@gmail.com>
2025-02-20 08:33:18 +0530

    madhu

--- backend/server.mjs
+++ backend/server.mjs
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config(); // Load environment variables
 
 const app = express();
-const PORT = 5001
+const PORT = 3000
 
 // Middleware
 app.use(express.json());
